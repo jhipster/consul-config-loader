9fd9a03155c2e6bd7425a7f0d3a921a39912f5c3
Aurélien Mino <aurelien.mino@gmail.com>
2020-09-27 11:24:09 +0200

    Add support for storing configuration as expected by Micronaut

--- upload-consul-file.sh
+++ upload-consul-file.sh
@@ -3,5 +3,12 @@
 file="$1"
 filename=$(basename $file)
 app=${filename%.*}
-curl  --output /dev/null -sX PUT --data-binary @$file http://$CONSUL_URL:$CONSUL_PORT/v1/kv/config/$app/data
-echo "   $file uploaded to Consul"
+if [[ "$ENABLE_SPRING" == "true" ]]; then
+  curl  --output /dev/null -sX PUT --data-binary @$file http://$CONSUL_URL:$CONSUL_PORT/v1/kv/config/$app/data
+  echo "   $file uploaded to Consul in Spring mode"
+fi
+
+if [[ "$ENABLE_MICRONAUT" == "true" ]]; then
+  curl  --output /dev/null -sX PUT --data-binary @$file http://$CONSUL_URL:$CONSUL_PORT/v1/kv/config/$app
+  echo "   $file uploaded to Consul in Micronaut mode"
+fi
